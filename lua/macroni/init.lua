1836ce98cfcd4553812c9462848bed8dc83f7472
kr40 <kr40.git@gmail.com>
2024-01-30 17:21:53 +0530

    Added check for default register
    If default register was "unnamedplus" it won't Yank to "+" if also do a setreg to "*" (I don't really know why), so this just check for default registers and Yanks accordingly. Tested and works fine for me.

--- lua/macroni/init.lua
+++ lua/macroni/init.lua
@@ -1,52 +1,58 @@
+local util = require("macroni.util")
 local M = {}
 
 local macros = {}
 
 function M.setup(opts)
-  macros = opts.macros or {}
+	macros = opts.macros or {}
 
--- Experimental, may change!
-  for key,macro in pairs(macros) do
-    if macro ~= 'string' and macro.keymap and macro.macro then
-      vim.keymap.set('n', macro.keymap, function () M.runSaved(key) end, {
-        desc = 'Saved macro: '..(macro.desc or key),
-        remap = true,
-      })
-    end
-  end
+	-- Experimental, may change!
+	for key, macro in pairs(macros) do
+		if macro ~= "string" and macro.keymap and macro.macro then
+			vim.keymap.set("n", macro.keymap, function()
+				M.runSaved(key)
+			end, {
+				desc = "Saved macro: " .. (macro.desc or key),
+				remap = true,
+			})
+		end
+	end
 end
 
 function M.yank(register)
-  register = register or vim.fn.input('Please specify a register to yank from: ')
-  vim.cmd.mode()
+	register = register or vim.fn.input("Please specify a register to yank from: ")
+	vim.cmd.mode()
 
-  local register_content = vim.fn.getreg(register)
+	local register_content = vim.fn.getreg(register)
 
-  if register == '' or register_content == '' then
-    print('Invalid register content!')
-    return
-  end
+	if register == "" or register_content == "" then
+		print("Invalid register content!")
+		return
+	end
 
-  local macro = vim.fn.keytrans(register_content)
-  vim.fn.setreg('+', macro)
-  vim.fn.setreg('*', macro)
-  vim.fn.setreg('"', macro)
-
-  print('Yanked macro from register `'..register..'`')
+	local macro = vim.fn.keytrans(register_content)
+	if util.get_default_register() == "+" then
+		vim.fn.setreg("+", macro)
+		vim.fn.setreg('"', macro)
+	else
+		vim.fn.setreg("*", macro)
+		vim.fn.setreg('"', macro)
+	end
+	print("Yanked macro from register `" .. register .. "`")
 end
 
 function M.run(macro)
-  vim.cmd.normal(vim.api.nvim_replace_termcodes(macro, true, true, true))
+	vim.cmd.normal(vim.api.nvim_replace_termcodes(macro, true, true, true))
 end
 
 -- Experimental, may change!
 -- TODO: Build telescope picker to easily select a saved macro
 function M.runSaved(key)
-  local macro = macros[key]
-  if type(macro) == 'string' then
-    return M.run(macro)
-  end
-  return M.run(macro.macro)
+	local macro = macros[key]
+	if type(macro) == "string" then
+		return M.run(macro)
+	end
+	return M.run(macro.macro)
 end
 
 return M
---
+++ lua/macroni/util.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+function M.get_default_register()
+	local clipboardFlags = vim.split(vim.api.nvim_get_option("clipboard"), ",")
+	if vim.tbl_contains(clipboardFlags, "unnamedplus") then
+		return "+"
+	end
+	if vim.tbl_contains(clipboardFlags, "unnamed") then
+		return "*"
+	end
+	return '"'
+end
+
+return M
